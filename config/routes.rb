8eea6e3268227d58feafa34d424a83f0c7e714e8
Daniel Le Cornu <dlec2@hotmail.co.uk>
2015-09-27 00:36:24 +0100

    styling and root changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
   resources :posts
   resources :doctors
   devise_for :users
+  devise_scope :user do 
+    root 'devise/sessions#new'
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

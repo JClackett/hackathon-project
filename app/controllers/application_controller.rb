61fd25b6f293093312f0fb1e89de1625f89a93f8
George Borg <georgeborg00@hotmail.com>
2015-09-26 19:14:10 +0100

    stashies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
   def configure_devise_permitted_parameters
     registration_params = [:first_name, :doctor, :last_name, :email, :password, :password_confirmation]
 

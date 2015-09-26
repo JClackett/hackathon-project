cfb399e5633dd525c603ac2938505564d0ae20c8
Jack Clackett <jclackett@gmail.com>
2015-09-26 15:55:22 +0100

    adding stuff to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926142835) do
+ActiveRecord::Schema.define(version: 20150926143231) do
 
   create_table "doctors", force: :cascade do |t|
     t.integer  "user_id"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150926142835) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "doctor"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

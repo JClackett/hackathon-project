b2ff45672edf186b1f57fc629fefd25bbde5b78e
Jack Clackett <jclackett@gmail.com>
2015-09-27 06:02:37 +0100

    making more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,15 @@ users_list = [
   { first_name: "Betty", last_name: "Stratford",email: 'patient1@gmail.com', password: 'password'},
   { first_name: "Stanley", last_name: "Rimple",email: 'patient2@gmail.com', password: 'password'},
   { first_name: "Stannis", last_name: "Baratheon",email: 'patient3@gmail.com', password: 'password'},
-
+  { first_name: "Rob", last_name: "Stark",email: 'patient4@gmail.com', password: 'password'},
+  { first_name: "Cersei", last_name: "Lannister",email: 'patient5@gmail.com', password: 'password'},
+  { first_name: "Peter", last_name: "Dinklage",email: 'patient6@gmail.com', password: 'password'},
+  { first_name: "Samwise", last_name: "Gamgee",email: 'patient7@gmail.com', password: 'password'},
+  { first_name: "Bob", last_name: "Splinger",email: 'patient8@gmail.com', password: 'password'},
+  { first_name: "Benois", last_name: "Le Hateau",email: 'patient9@gmail.com', password: 'password'},
+  { first_name: "Cesar", last_name: "Spinzwazar",email: 'patient10@gmail.com', password: 'password'},
+  { first_name: "Eduardo", last_name: "Framptoni",email: 'patient11@gmail.com', password: 'password'},
+  { first_name: "David", last_name: "Finkbrau",email: 'patient12@gmail.com', password: 'password'},
 ]
 
 users_list.each do |user|
@@ -33,7 +41,15 @@ patients_list = [
   { user_id: '3', doctor_id: '1'},
   { user_id: '4', doctor_id: '2'},
   { user_id: '5', doctor_id: '2'},
-
+  { user_id: '6', doctor_id: '1'},
+  { user_id: '7', doctor_id: '1'},
+  { user_id: '8', doctor_id: '2'},
+  { user_id: '9', doctor_id: '2'},
+  { user_id: '10', doctor_id: '1'},
+  { user_id: '11', doctor_id: '2'},
+  { user_id: '12', doctor_id: '1'},
+  { user_id: '13', doctor_id: '2'},
+  { user_id: '14', doctor_id: '1'},
 ]
 
 patients_list.each do |patient|
@@ -41,8 +57,14 @@ patients_list.each do |patient|
 end
 
 posts_list = [
-  { patient_id: '1', doctor_id: '1', location: 'head', discomfort: '7', frequency: 'intermittent', initial_occurence: 'last week', notes: "I've had a pretty bad headache on and off for a week"},
-  { patient_id: '2', doctor_id: '2', location: 'throat', discomfort: '2', frequency: 'constant', initial_occurence: 'yesterday morning', notes: "Woke up yesterday with a very tickly cough"},
+  { patient_id: '1', doctor_id: '1', location: 'Head', discomfort: '7', frequency: 'Intermittent', initial_occurence: 'Last Week', notes: "I've had a pretty bad headache on and off for a week"},
+  { patient_id: '2', doctor_id: '2', location: 'Throat', discomfort: '2', frequency: 'Constant', initial_occurence: 'Yesterday Morning', notes: "Woke up yesterday with a very tickly cough"},
+  { patient_id: '2', doctor_id: '2', location: 'Neck', discomfort: '5', frequency: 'Constant', initial_occurence: 'Two Days Ago', notes: "Woke up with extremely sore neck"},
+  { patient_id: '2', doctor_id: '2', location: 'Throat', discomfort: '2', frequency: 'Constant', initial_occurence: 'Yesterday Morning', notes: "Woke up yesterday with a very tickly cough"},
+  { patient_id: '2', doctor_id: '2', location: 'Throat', discomfort: '2', frequency: 'Constant', initial_occurence: 'Yesterday Morning', notes: "Woke up yesterday with a very tickly cough"},
+  { patient_id: '2', doctor_id: '2', location: 'Throat', discomfort: '2', frequency: 'Constant', initial_occurence: 'Yesterday Morning', notes: "Woke up yesterday with a very tickly cough"},
+  { patient_id: '2', doctor_id: '2', location: 'Throat', discomfort: '2', frequency: 'Constant', initial_occurence: 'Yesterday Morning', notes: "Woke up yesterday with a very tickly cough"},
+
 ]
 
 posts_list.each do |post|
